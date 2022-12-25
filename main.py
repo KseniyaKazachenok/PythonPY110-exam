c7a8085ffa4e03c4c3d8d731e869245d509e3197
KseniyaKazachenok <115838238+KseniyaKazachenok@users.noreply.github.com>
2022-12-25 21:10:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ fake = faker.Faker("ru")
 output_file = "output.json"
 
 
-def main():
+def main() -> None:
+    """
+    Записывает генерируемые словари в json файл
+    :return: json файл
+    """
     for _ in range(100):
         with open(output_file, "w", encoding="utf-8") as f:
             json.dump(next(dict_books), f, indent=4, ensure_ascii=False)
 
 
 def generator():
+    """
+    Генерирует словарь, содержащий информацию о книге
+    :return: словарь
+    """
     dict_ = {
         "model": conf.MODEL,
         "pk": next(counter),
@@ -34,7 +42,11 @@ def generator():
 dict_books = generator()
 
 
-def title_name():
+def title_name() -> str:
+    """
+    Возвращает название книги
+    :return: название книги
+    """
     filename = "books.txt"
     with open(filename, "w", encoding="utf-8") as f:
         for _ in range(5):
@@ -48,34 +60,58 @@ def title_name():
     return title_str
 
 
-def year_name():
+def year_name() -> int:
+    """
+    Возвращает год издания книги
+    :return: год
+    """
     year = random.randint(1950, 2022)
     return year
 
 
-def pages_name():
+def pages_name() -> int:
+    """
+    Возвращает количество страниц в книге
+    :return: количество страниц
+    """
     pages = random.randint(10, 3000)
     return pages
 
 
-def isbn_name():
+def isbn_name() -> str:
+    """
+    Возвращает уникальный идентификационный номер издания
+    :return: isbn13
+    """
     isbn13 = fake.isbn13()
     return isbn13
 
 
-def rating_name():
+def rating_name() -> float:
+    """
+    Возвращает рейтинг книги
+    :return: рейтинг
+    """
     rating = random.uniform(0, 5)
     rating_round = round(rating, 2)
     return rating_round
 
 
-def price_name():
+def price_name() -> float:
+    """
+    Возвращает цену книги
+    :return: цена
+    """
     price = random.uniform(1, 5000)
     price_round = round(price, 2)
     return price_round
 
 
-def author_name():
+def author_name() -> list:
+    """
+    Возвращает имена и фамилии авторов (от 1 до 3)
+    :return: список авторов
+    """
     author_start = []
     author_number = random.randint(1, 3)
     for _ in range(author_number):
@@ -86,6 +122,10 @@ def author_name():
 
 
 def step():
+    """
+    Автоинкремент
+    :return:
+    """
     step_ = 1
     while True:
         yield step_
